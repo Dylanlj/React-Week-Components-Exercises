75c3a98054ff7c260aaad95b6a4016d99b7ec4f3
JoelCodes <amnshinness@yahoo.com>
2018-03-10 12:04:25 -0800

    Changed exercises to reflect curriculum QA and fixed rando.js

--- rando.js
+++ rando.js
@@ -3,11 +3,13 @@ var rando = (function(){
   var letters = 'abcdefghijklmnopqrstuvwxyz';
   var alphabet = `0123456789${letters}${letters.toUpperCase()}`;
 
-  function rando(){
+  return function rando(){
     var output = '';
+
     for (var i = 0; i < 6; i += 1) {
       output += alphabet[Math.floor(Math.random() * alphabet.length)];
     }
+
     return output;
   };
 }());
